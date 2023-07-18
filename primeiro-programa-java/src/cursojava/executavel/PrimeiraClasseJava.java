06f1112e4d7bebdeec62adfceef58d576c44ed16
yagobotelho <yagobotelho11@gmail.com>
2023-07-18 21:32:27 +0100

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java - do CURSO Formação JAVA WEB");
+	}
+}
